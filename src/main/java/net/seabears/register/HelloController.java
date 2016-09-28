c67d30ac4131a8b25a3ddf542a51c1cf777f74f3
Corey Beres <corey.beres@gmail.com>
2016-09-27 22:51:06 -0400

    followed Spring's getting-started guide

---
+++ src/main/java/net/seabears/register/HelloController.java
@@ -0,0 +1,13 @@
+package net.seabears.register;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
+
