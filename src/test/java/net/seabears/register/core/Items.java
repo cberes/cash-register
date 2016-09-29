e8e99214cb198e7466d2513f2ecc7cb06f79cd22
Corey Beres <corey.beres@gmail.com>
2016-09-29 19:29:15 -0400

    test couchbase data-store implementation

--- src/test/java/net/seabears/register/core/Items.java
+++ src/test/java/net/seabears/register/core/Items.java
@@ -1,5 +1,12 @@
 package net.seabears.register.core;
 
+import com.couchbase.client.java.document.json.JsonObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
 import static org.mockito.BDDMockito.*;
 
 public final class Items {
@@ -33,6 +40,50 @@ public final class Items {
             @Override
             public int getPrice() {
                 return price;
+            }
+        };
+    }
+
+    public static JsonObject itemJson(final int id, final String name, final int price) {
+        return itemJson(item(id, name, price));
+    }
+
+    public static JsonObject itemJson(final Item item) {
+        final JsonObject json = JsonObject.empty();
+        json.put("id", item.getId());
+        json.put("name", item.getName());
+        json.put("price", item.getPrice());
+        return json;
+    }
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static Matcher<Item> isItem(final Item item) {
+        return new BaseMatcher<Item>() {
+            @Override
+            public boolean matches(Object other) {
+                return other instanceof Item
+                        && ((Item) other).getId() == item.getId()
+                        && ((Item) other).getName().equals(item.getName())
+                        && ((Item) other).getPrice() == item.getPrice();
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                try {
+                    description.appendText("Expected ").appendValue(mapper.writeValueAsString(item));
+                } catch (JsonProcessingException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+
+            @Override
+            public void describeMismatch(Object other, Description description) {
+                try {
+                    description.appendText("was ").appendValue(mapper.writeValueAsString(other));
+                } catch (JsonProcessingException e) {
+                    throw new IllegalStateException(e);
+                }
             }
         };
     }
--- src/test/java/net/seabears/register/couchbase/DataStoreTest.java
+++ src/test/java/net/seabears/register/couchbase/DataStoreTest.java
@@ -1,24 +1,173 @@
 package net.seabears.register.couchbase;
 
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.JsonLongDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.query.Query;
+import com.couchbase.client.java.query.QueryResult;
+import com.couchbase.client.java.query.QueryRow;
+import net.seabears.register.core.Items;
+import net.seabears.register.core.OrderConfig;
+import net.seabears.register.core.Orders;
+import net.seabears.register.core.Payment;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static net.seabears.register.core.Items.isItem;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ImportAutoConfiguration
 public class DataStoreTest {
+    @Autowired
+    private DataStore data;
+
     @MockBean
     private Bucket bucket;
 
+    @MockBean
+    private QueryResult result;
+
+    @MockBean
+    private OrderIdSupplier orderIdSupplier;
+
+    @MockBean
+    private PaymentIdSupplier paymentIdSupplier;
+
+    private static QueryRow mockRow(JsonObject value) {
+        final QueryRow row = mock(QueryRow.class);
+        given(row.value()).willReturn(value);
+        return row;
+    }
+
+    private static JsonDocument mockDoc(JsonObject value) {
+        final JsonDocument doc = mock(JsonDocument.class);
+        given(doc.content()).willReturn(value);
+        return doc;
+    }
+
+    @Before
+    public void setup() {
+        given(bucket.name()).willReturn("test-bucket");
+        given(bucket.query(any(Query.class))).willReturn(result);
+    }
+
+    private static void allItemsMatch(List<net.seabears.register.core.Item> expected, List<net.seabears.register.core.Item> actual) {
+        range(0, expected.size()).forEach(i -> assertThat(actual.get(i), isItem(expected.get(i))));
+    }
+
     @Test
-    public void test() {
+    public void getItems() {
+        List<net.seabears.register.core.Item> items = Arrays.asList(Items.item(1, "foo", 100), Items.item(2, "bar", 250));
+        List<QueryRow> rows = items.stream().map(Items::itemJson).map(DataStoreTest::mockRow).collect(toList());
+        given(result.allRows()).willReturn(rows);
+        allItemsMatch(data.getItems(), items);
+    }
+
+    @Test
+    public void getItem() {
+        final net.seabears.register.core.Item item = Items.item(1, "foo", 100);
+        final JsonDocument doc = mockDoc(Items.itemJson(item));
+        given(bucket.get("item_1")).willReturn(doc);
+        assertThat(data.getItem(1), isItem(item));
+    }
+
+    @Test
+    public void getItemNotFound() {
+        given(bucket.get(anyString())).willReturn(null);
+        assertThat(data.getItem(1), nullValue());
+    }
+
+    @Test
+    public void createPayment() {
+        final String id = "51";
+        final Payment expected = new Payment();
+        expected.amount = 100;
+        expected.method = "CASH";
+        expected.orderId = "foo";
+        given(paymentIdSupplier.get()).willReturn(id);
+        data.createPayment(expected);
+        ArgumentCaptor<JsonDocument> doc = ArgumentCaptor.forClass(JsonDocument.class);
+        verify(bucket).insert(doc.capture());
+        assertThat(doc.getValue().content().getString("id"), equalTo(id));
+        assertThat(doc.getValue().content().getInt("amount"), equalTo(expected.amount));
+        assertThat(doc.getValue().content().getString("method"), equalTo(expected.method));
+        assertThat(doc.getValue().content().getString("order_id"), equalTo("order_" + expected.orderId));
+    }
+
+    @Test
+    public void createOrder() {
+        final String id = "51";
+        final double tax = 0.09;
+        final OrderConfig expected = Orders.config(tax);
+        given(orderIdSupplier.get()).willReturn(id);
+        final Order actual = data.createOrder(expected);
+        ArgumentCaptor<JsonDocument> doc = ArgumentCaptor.forClass(JsonDocument.class);
+        verify(bucket).insert(doc.capture());
+        assertThat(doc.getValue().content().getString("id"), equalTo(id));
+        assertEquals(doc.getValue().content().getDouble("tax"), tax, 1E-9);
+        assertThat(actual.getId(), equalTo(id));
+        assertThat(actual.isSubmitted(), equalTo(false));
+    }
+
+    @Test
+    public void getOrder() {
+        final JsonObject expected = JsonObject.empty();
+        final JsonDocument doc = mockDoc(expected);
+        given(bucket.get("order_1")).willReturn(doc);
+        assertThat(data.getOrder("1").json, is(expected));
+    }
+
+    @Test
+    public void getOrderNotFound() {
+        given(bucket.get(anyString())).willReturn(null);
+        assertThat(data.getOrder("1"), nullValue());
+    }
+
+    @Test
+    public void updateOrder() {
+        final String id = "51";
+        final JsonObject json = JsonObject.empty();
+        data.updateOrder(id, new Order(json));
+        ArgumentCaptor<JsonDocument> doc = ArgumentCaptor.forClass(JsonDocument.class);
+        verify(bucket).upsert(doc.capture());
+        assertThat(doc.getValue().id(), equalTo("order_" + id));
+        assertThat(doc.getValue().content(), is(json));
+    }
+
+    @Test
+    public void incrementAndGetOrderNumber() {
+        final JsonLongDocument json = mock(JsonLongDocument.class);
+        given(bucket.counter("order_number", 1)).willReturn(json);
+        given(json.content()).willReturn(0L);
+        assertThat(data.incrementAndGetOrderNumber(), equalTo(1));
+        given(json.content()).willReturn(1L);
+        assertThat(data.incrementAndGetOrderNumber(), equalTo(2));
+        given(json.content()).willReturn(99L);
+        assertThat(data.incrementAndGetOrderNumber(), equalTo(100));
+        given(json.content()).willReturn(100L);
+        assertThat(data.incrementAndGetOrderNumber(), equalTo(1));
+        given(json.content()).willReturn(101L);
+        assertThat(data.incrementAndGetOrderNumber(), equalTo(2));
     }
 }
 
